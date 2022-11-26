7d9f1f3c09c3b2c861b6b845a9ddcebcc8f17183
Savimasima <savimasima@gmail.com>
2022-11-26 18:08:07 +0000

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="">Coded by Savimasima</footer>
+      </div>
+    </div>  
   );
 }
 
