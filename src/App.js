0889ecac06f42b0116b8cd4a3f1692a10522ca1f
Savimasima <savimasima@gmail.com>
2022-11-27 21:18:15 +0000

    Added definitions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
+import "./App.css";
 import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          savimasima
+        </header>
         <main>
           <Dictionary />
         </main>
-        <footer className="">Coded by Savimasima</footer>
+        <footer className="App-footer">
+          <small>Coded by savimasima</small>
+        </footer>
       </div>
-    </div>  
+    </div>
   );
 }
-
-export default App;
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,31 +1,34 @@
 import React, { useState } from "react";
-import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
-    function handleResponse(response) {
-        console.log(response);
-    }
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
-    function search(event) {
-        event.preventDefault();
-        alert(`Searching for ${keyword}`);
+  function search(event) {
+    event.preventDefault();
 
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
-    }
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    function handleKeywordChange(event) {
-        setKeyword(event.target.value);
-    }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-    return (
-        <div className="Dictionary">
-            <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange} />
-            </form>
-        </div>
-    );
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
 }
