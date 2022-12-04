70526b1bc44ffd05023baf7d903ffc6b19eb8904
Savimasima <savimasima@gmail.com>
2022-12-04 17:16:01 +0000

    Added synonyms

---
+++ src/Synonyms.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Synonyms(props) {
+    if (props.synonyms) {
+        return (
+            <div>
+                <ul className="Synonyms">
+                    {props.synonyms.map(function (synonym, index) {
+                        return (
+                            <li key={index}>
+                                {synonym}
+                            </li>
+                        )
+                    })}
+                </ul>  
+            </div>  
+        ); 
+    } else {
+        return null;
+    }
+    
+}
